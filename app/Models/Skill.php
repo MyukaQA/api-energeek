772b587859cc93c655e7885fadcd6ac24e3d38a7
MyukaQA <mylian214@gmail.com>
2023-07-05 11:58:27 +0700

    create db

---
+++ app/Models/Skill.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+class Skill extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'skills';
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    protected $guarded = [''];
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->created_by = Auth::id();
+        });
+
+        static::updating(function ($model) {
+            $model->updated_by = Auth::id();
+        });
+
+        static::deleting(function ($model) {
+            $model->deleted_by = Auth::id();
+            $model->save();
+        });
+    }
+}
